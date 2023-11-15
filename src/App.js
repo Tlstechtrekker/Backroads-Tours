016177d97c3bb155a2516cffbf659c58a7bf1d74
Tlstechtrekker <tschultzphd@gmail.com>
2023-11-14 23:35:09 -0700

    created Title component - added into each section

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import About from './components/About';
 import Services from './components/Services';
 import Tours from './components/Tours';
 import Footer from './components/Footer';
+import TroubleshootAbout from './components/TroubleshootAbout';
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      {/* <TroubleshootAbout /> */}
       <About />
       <Services />
       <Tours />
